9251a95f81b78cb0d9a57aa948a409974201697d
Thiago de Almeida Correia Fernandes <110740750+thiagoalmeidafernandes@users.noreply.github.com>
2022-11-17 21:37:06 -0300

    Finalizando em HTTP params
    Finalizando em HTTP params, na intenção de continuar no futuro, preciso realizar algumas coisas antes

--- angularhttp/src/app/services/user.service.ts
+++ angularhttp/src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 import { environment } from 'src/environments/environment';
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class UserService {
   private apiUrl = environment.apiUrl;
+  //2º -- readonly moreParams = ['test', 'test2'];
 
   constructor(private http: HttpClient) {}
 
@@ -17,12 +18,26 @@ export class UserService {
   }
 
   getUsers(): Observable<User[]> {
-    let myHeaders = new HttpHeaders({ myheader: 'headervalue' });
-    myHeaders = myHeaders.set('id', '1234');
-    myHeaders = myHeaders.append('id', '0001');
-    return this.http.get<User[]>(`${this.apiUrl}/users`, {
-      headers: myHeaders,
-    });
+    //-----HTTP Headers-------
+    //let myHeaders = new HttpHeaders({ myheader: 'headervalue' });
+    //myHeaders = myHeaders.set('id', '1234');
+    //myHeaders = myHeaders.append('id', '0001');
+    //return this.http.get<User[]>(`${this.apiUrl}/users`, {
+    //  headers: myHeaders,
+    //});
+
+    //-----HTTP Params--------
+    //1st way -- let myParams = new HttpParams({}).set('page', '5').set('sort', 'true');
+    //1 -- myParams = myParams.append('name', 'thiago');
+    //1 -- myParams = myParams.append('name', 'maria');
+
+    //2nd way -- const theParams = { ['testList']: this.moreParams };
+    //2º -- let myParams = new HttpParams({ fromObject: theParams });
+
+    //return this.http.get<User[]>(`${this.apiUrl}/users`, { params: myParams,});
+
+    //-----Default------------
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
   }
 
   createUser(payload: User): Observable<User> {
